5ef605f7620f560a3099b2d8fc6c4771acf3dfff
Mrityunjay Jha <mrityu.jha@gmail.com>
2021-02-17 17:40:45 +0530

    MAKING CONFIG FILE MORE GENERALISED
    Using 'os' module, 'paths' defined in the config file has been been made more easy to be transferred to other systems. Only 'ROOT_DIR' needs to be changed now.

--- src/config.py
+++ src/config.py
@@ -1,13 +1,20 @@
 import tensorflow as tf
 import keras 
+import os 
 
-
-TRAIN_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/combined_train/"
-CSV_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/train.csv"
-TEST_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/test/"
-INPUT = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/"
-IMG_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/train/"
-SAVE_MODEL = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/models/"
+ROOT_DIR = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition"
+INPUT = os.path.join( ROOT_DIR, 'input' ) 
+#"M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/"
+TRAIN_PATH = os.path.join( INPUT, 'combined_train' )    
+#"M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/combined_train/"
+CSV_PATH = os.path.join( INPUT, 'train.csv' )
+#CSV_PATH = os."M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/train.csv"
+TEST_PATH = os.path.join( INPUT, 'test' )
+#TEST_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/test/"
+IMG_PATH = os.path.join( INPUT, 'train' )
+#IMG_PATH = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/input/train/"
+SAVE_MODEL = os.path.join( INPUT, 'models' )
+#SAVE_MODEL = "M:/My Files/Mrityunjay Jha/Programming/MIBM Lab/Facial Expression Recognition/models/"
 
 BATCH_SIZE = 64
 TARGET_SIZE = ( 224, 224, 3 )
